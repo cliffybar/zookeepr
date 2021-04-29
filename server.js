996660cfdd567dd2f4657f00c6cad24aac382061
cliffybar <crowellclifford@utexas.edu>
2021-04-28 23:27:42 -0500

    added a new get function

--- server.js
+++ server.js
@@ -39,6 +39,11 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -48,6 +53,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(animals);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
